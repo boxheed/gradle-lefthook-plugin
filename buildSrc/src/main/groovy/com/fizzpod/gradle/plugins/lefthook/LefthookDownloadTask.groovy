8c306cf1f6cb22ea6e44a43c375ec8efb11c6f99
Andrew Dunn <boxheed@users.noreply.github.com>
2025-01-15 22:29:26 +0000

    feat: added ttl caching to lefthook binary discovery

--- buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/lefthook/LefthookDownloadTask.groovy
+++ buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/lefthook/LefthookDownloadTask.groovy
@@ -40,9 +40,52 @@ public class LefthookDownloadTask extends DefaultTask {
     static def run = Loggy.wrap({ context ->
         return Optional.ofNullable(context)
             .map(x -> LefthookDownloadTask.location(x))
+            .map(x -> LefthookDownloadTask.ttl(x))
             .map(x -> LefthookDownloadTask.download(x))
             .orElseThrow(() -> new RuntimeException("Unable to install lefthook"))
     })
+
+    /**
+    * Find the most recent binary and see if it is within ttl
+    */
+    static def ttl = { x ->
+        def binary = x.extension.binary
+        if(binary == null || !binary.exists()) {
+            def location = x.location
+            def arch = OS.getArch(x.extension.arch)
+            def os = OS.getOs(x.extension.os)
+            def ttl = x.extension.ttl
+            binary = LefthookDownloadTask.resolveTtl(location, arch, os, ttl)
+        }
+        if(binary != null && binary.exists()) {
+            x.extension.binary = binary
+            x.binary = binary
+        }
+        return x
+    }
+
+    static def resolveTtl = {  File location, OS.Arch arch, OS.Family os, long ttl ->
+        def latestBinary = null
+        def currentTime = System.currentTimeMillis()
+        def binaryPattern = LefthookInstallation.getBinaryName("v?(\\d+\\.\\d+\\.\\d+)", os, arch) + ".*"
+        location.listFiles().each { File file ->
+            if (file.name =~ binaryPattern) {
+                Loggy.debug("Checking ${file.name}")
+                def lastModified = file.lastModified()
+                def timeDiff = currentTime - lastModified
+                if (timeDiff < ttl) { 
+                    Loggy.debug("${file.name} within ttl of ${ttl}")
+                    if(latestBinary != null && latestBinary.lastModified() < file.lastModified()) {
+                        latestBinary = file
+                    } else if (latestBinary == null){
+                        latestBinary = file
+                    }
+                }
+            }
+        }
+        Loggy.lifecycle("found lefthook binary: ${latestBinary}")
+        return latestBinary
+    }
 
     static def download = Loggy.wrap({ x ->
         def repo = x.extension.repository
@@ -50,7 +93,9 @@ public class LefthookDownloadTask extends DefaultTask {
         def os = x.extension.os
         def version = x.extension.version
         def location = x.location
-        x.binary = LefthookInstallation.install(repo, arch, os, version, location)
+        if(!x.binary || !x.binary.exists()) {
+            x.binary = LefthookInstallation.install(repo, arch, os, version, location)
+        }
         return x.binary? x: null
     })
 
