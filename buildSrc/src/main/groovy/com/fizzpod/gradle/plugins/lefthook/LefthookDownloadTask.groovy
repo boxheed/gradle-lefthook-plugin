bde0ad4af60857ac20ea88791eeede071a3ed46e
Andrew Dunn <boxheed@users.noreply.github.com>
2026-05-05 18:49:38 +0000

    fix: moved the location of the lefthook version file to prevent build cleaning removing the file

--- buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/lefthook/LefthookDownloadTask.groovy
+++ buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/lefthook/LefthookDownloadTask.groovy
@@ -42,19 +42,19 @@ public abstract class LefthookDownloadTask extends DefaultTask {
         getLefthookLocation().convention(extension.getLocation())
 
         getLefthookBinary()
-                .set(
-                        getResolvedVersionFile()
-                                .zip(
-                                        getLefthookLocation(),
-                                        { versionFile, location ->
-                                            def version = versionFile.asFile.text.trim()
-                                            def os = OS.getOs(null)
-                                            def arch = OS.getArch(null)
-                                            def name =
-                                                    LefthookInstallation.getBinaryName(
-                                                            version, os, arch)
-                                            return location.file(name)
-                                        }))
+            .set(getResolvedVersionFile()
+                .zip(getLefthookLocation(),
+                    { versionFile, location ->
+                        def version = versionFile.asFile.text.trim()
+                        def os = OS.getOs(null)
+                        def arch = OS.getArch(null)
+                        def name =
+                                LefthookInstallation.getBinaryName(
+                                        version, os, arch)
+                        return location.file(name)
+                    }
+                )
+            )
     }
 
     static register(Project project) {
